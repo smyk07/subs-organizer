1c7b86b6914dfb055c9d530d9abbf8deb4be8502
smyk07 <samyak.bambole07@gmail.com>
2024-06-13 20:42:48 +0530

    Bash Script setup

--- subs_organizer.sh
+++ subs_organizer.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+echo "Downloading script.py..."
+
+curl -o script.py https://raw.githubusercontent.com/smyk07/subs-organizer/master/script.py
+
+echo --------------------
+
+python3 ./script.py
+
+echo --------------------
+
+read -p "Do you want to delete the script.py file? (y/n) " yn
+
+case $yn in
+y) rm ./script.py ;;
+n) exit ;;
+*)
+	echo invalid response...
+	exit 1
+	;;
+esac
